36dff93e44e7b7bc04636469f5fcc80d0171c172
trizzle21 <tdross21@gmail.com>
2020-12-05 14:28:54 -0500

    day_6/scaffold: added scaffold tooling and day_6

---
+++ day_template/part_1.py
@@ -0,0 +1,18 @@
+"""
+
+"""
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+LOG = logging.getLogger()
+
+
+def run() -> int:
+    with open('day_5_input.txt', 'r') as f:
+        lines = [str(value).strip('\n') for value in f.readlines()]
+    return len(lines)
+
+
+if __name__ == '__main__':
+    result = run()
+    print(f'result: {result}')
---
+++ scaffold_day.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+DAY=6
+day_package="day_${DAY}"
+
+cp -R day_template $day_package
+
+part_1_name="${day_package}_part_1.py"
+part_2_name="${day_package}_part_2.py"
+input_name="${day_package}_input.txt"
+
+mv $day_package/part_1.py $day_package/{$part_1_name}_part_1.py
+mv $day_package/part_2.py $day_package/$part_2_name
+mv $day_package/input.txt $day_package/$input_name
